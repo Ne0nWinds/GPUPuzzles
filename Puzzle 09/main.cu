73d282cfe25f8baa25fd013fd8785b014439e271
Caden Parker <cadenparker@ymail.com>
2025-02-22 12:27:54 -0500

    Adjust Puzzles 9 and 10

--- Puzzle 09/main.cu
+++ Puzzle 09/main.cu
@@ -2,76 +2,68 @@
 
 #include <stdio.h>
 
-static inline s32 S32_Max(s32 A, s32 B) {
-	return (A > B) ? A : B;
-}
-
-static random_state RandomState = { 0xB40148552A2E3491 };
 #define ARRAY_SIZE 8
 static u32 InitialArray[ARRAY_SIZE] = {0};
-static u32 ResultArray[ARRAY_SIZE * 2] = {0};
+static u32 ResultArray[ARRAY_SIZE] = {0};
 
-void InitRandomIntegers() {
+void Init() {
 	for (u32 i = 0; i < ARRAY_SIZE; ++i) {
 		InitialArray[i] = i;
 	}
 }
 
-__global__ void Pooling(u32 *In, u32 *Out, u32 Length) {
-	__shared__ u32 SharedMemory[8];
-	u32 Index = blockIdx.x * blockDim.x + threadIdx.x;
-
 #if 0
-	u32 Sum = 0;
-	for (u32 Offset = ((s32)Index - 2 > 0) ? (s32)Index - 2 : 0; Offset <= Index; ++Offset) {
-		Sum += In[Offset];
-	}
-#else
-	u32 Sum = 0;
-	SharedMemory[threadIdx.x] = In[Index];
-	__syncthreads();
-
-	for (u32 Offset = ((s32)Index - 2 > 0) ? (s32)Index - 2 : 0; Offset <= Index; ++Offset) {
-		Sum += SharedMemory[Offset];
-	}
-	Out[Index] = Sum;
-#endif
+__global__ void Pooling(u32 *A) {
+	u32 Index = threadIdx.x;
+	u32 Sum = A[Index];
+	if (Index >= 1) Sum += A[Index - 1];
+	if (Index >= 2) Sum += A[Index - 2];
+	A[Index] = Sum;
 }
+#elif 0
+__global__ void Pooling(u32 *A) {
+	u32 Index = threadIdx.x;
+
+	__shared__ u32 SharedMemory[8];
+	SharedMemory[threadIdx.x] = A[Index];
+	u32 Sum = SharedMemory[threadIdx.x];
+	__syncthreads();
+	if (threadIdx.x >= 1) Sum += SharedMemory[threadIdx.x - 1];
+	if (threadIdx.x >= 2) Sum += SharedMemory[threadIdx.x - 2];
+
+	A[Index] = Sum;
+}
+#else
+__global__ void Pooling(u32 *A) {
+	u32 Index = threadIdx.x;
+	u32 SharedValue = A[Index];
+	u32 Sum = SharedValue;
+	Sum += __shfl_up_sync(0xFFFFFFFF, SharedValue, 1) * (Index >= 1);
+	Sum += __shfl_up_sync(0xFFFFFFFF, SharedValue, 2) * (Index >= 2);
+	A[Index] = Sum;
+}
+#endif
 
 s32 main() {
-	InitRandomIntegers();
-	puts("===");
+	Init();
 
-	u32 *GPUArray1 = 0, *GPUArray2 = 0;
-	cudaMalloc(&GPUArray1, ARRAY_SIZE * sizeof(s32));
-	cudaMalloc(&GPUArray2, sizeof(ResultArray));
-	cudaMemset(GPUArray2, 0, sizeof(ResultArray));
-	cudaMemcpy(GPUArray1, InitialArray, ARRAY_SIZE * sizeof(s32), cudaMemcpyHostToDevice);
+	u32 *GPUArray = 0;
+	u32 SizeInBytes = sizeof(InitialArray);
+	cudaMalloc(&GPUArray, SizeInBytes);
+	cudaMemcpy(GPUArray, InitialArray, SizeInBytes, cudaMemcpyHostToDevice);
 
-	dim3 threadDimension(8, 1);
-	dim3 blockDimension(1, 1);
-	Pooling<<<blockDimension, threadDimension>>>(GPUArray1, GPUArray2, ARRAY_SIZE);
-	cudaMemcpy(ResultArray, GPUArray2, sizeof(ResultArray), cudaMemcpyDeviceToHost);
+	Pooling<<<1, 8>>>(GPUArray);
+	cudaMemcpy(ResultArray, GPUArray, SizeInBytes, cudaMemcpyDeviceToHost);
 
-	puts("===");
-
-	u32 i = 0;
-	for (; i < ARRAY_SIZE; ++i) {
-		u32 ExpectedResult = 0;
-		for (u32 j = S32_Max((s32)i - 2, 0); j <= i; ++j) {
-			ExpectedResult += InitialArray[j];
-		}
-		u32 ActualResult = ResultArray[i];
-		if (ExpectedResult == ActualResult) {
-			printf(ANSI_COLOR_GREEN "Expected: %u -- Actual: %u\n", ExpectedResult, ActualResult);
+	for (u32 i = 0; i < ARRAY_SIZE; ++i) {
+		u32 Expected = InitialArray[i];
+		if (i >= 1) Expected += InitialArray[i - 1];
+		if (i >= 2) Expected += InitialArray[i - 2];
+		u32 Actual = ResultArray[i];
+		if (Expected == Actual) {
+			printf(ANSI_COLOR_GREEN "Expected: %u -- Actual: %u\n", Expected, Actual);
 		} else {
-			printf(ANSI_COLOR_RED "Expected: %u -- Actual: %u\n", ExpectedResult, ActualResult);
-		}
-	}
-	for (; i < ARRAY_SIZE * 2; ++i) {
-		if (ResultArray[i] != 0) {
-			printf(ANSI_COLOR_RED "Wrote past the end of the array at index: %u", i);
-			break;
+			printf(ANSI_COLOR_RED "Expected: %u -- Actual: %u\n", Expected, Actual);
 		}
 	}
 	printf(ANSI_COLOR_RESET);
--- Puzzle 10/main.cu
+++ Puzzle 10/main.cu
@@ -2,85 +2,116 @@
 
 #include <stdio.h>
 
-static random_state RandomState = { 0xB40148552A2E3491 };
-#define ARRAY_SIZE 8 * 1024 * 1024
+__device__ f32 ReduceAdd(f32 Value) {
+	u32 Mask = 0xFFFFFFFF;
+	Value += __shfl_down_sync(Mask, Value, 16);
+	Value += __shfl_down_sync(Mask, Value, 8);
+	Value += __shfl_down_sync(Mask, Value, 4);
+	Value += __shfl_down_sync(Mask, Value, 2);
+	Value += __shfl_down_sync(Mask, Value, 1);
+	return Value;
+}
+
+#if 0
+#define ARRAY_SIZE 32
+// Thread-level dot product
+__global__ void Dot(f32 *A, f32 *B, f32 *Out) {
+
+	f32 ElementwiseProduct = A[threadIdx.x] * B[threadIdx.x];
+	f32 ReducedSum = ReduceAdd(ElementwiseProduct);
+
+	if (threadIdx.x == 0) {
+		*Out = ReducedSum;
+	}
+}
+#elif 0
+#define ARRAY_SIZE 1024
+// Block-level dot product
+__global__ void Dot(f32 *A, f32 *B, f32 *Out) {
+
+	__shared__ f32 WarpLevelResults[32];
+
+	f32 ElementwiseProduct = A[threadIdx.x] * B[threadIdx.x];
+	f32 ReducedSum = ReduceAdd(ElementwiseProduct);
+
+	u32 WarpIndex = threadIdx.x / 32;
+	if (threadIdx.x % 32 == 0) {
+		WarpLevelResults[WarpIndex] = ReducedSum;
+	}
+	__syncthreads();
+
+	if (WarpIndex == 0) {
+		f32 Result = WarpLevelResults[threadIdx.x];
+		Result = ReduceAdd(Result);
+		if (threadIdx.x == 0) {
+			*Out = Result;
+		}
+	}
+}
+#else
+#define ARRAY_SIZE 2048
+// Global dot product
+__global__ void Dot(f32 *A, f32 *B, f32 *Out) {
+
+	__shared__ f32 WarpDotProductResults[32];
+
+	u32 GlobalIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	f32 ElementwiseProduct = A[GlobalIndex] * B[GlobalIndex];
+	f32 ReducedSum = ReduceAdd(ElementwiseProduct);
+
+	u32 WarpIndex = threadIdx.x / 32;
+	WarpDotProductResults[WarpIndex] = ReducedSum;
+	__syncthreads();
+
+	if (WarpIndex == 0) {
+		f32 Result = WarpDotProductResults[threadIdx.x];
+		Result = ReduceAdd(Result);
+		if (threadIdx.x == 0) {
+			atomicAdd(Out, Result);
+		}
+	}
+}
+#endif
+
 static f32 InitialArray1[ARRAY_SIZE] = {0};
 static f32 InitialArray2[ARRAY_SIZE] = {0};
-static f32 ResultArray[ARRAY_SIZE / 8] = {0};
+static f32 Result = 0.0f;
 
-void InitRandomIntegers() {
+static void Init() {
+	random_state RandomState = { 0xB40148552A2E3491ULL };
 	for (u32 i = 0; i < ARRAY_SIZE; ++i) {
-		InitialArray1[i] = RandomInt(&RandomState) % 32;
-		InitialArray2[i] = RandomInt(&RandomState) % 32;
+		InitialArray1[i] = RandomFloat(&RandomState);
+		InitialArray2[i] = RandomFloat(&RandomState);
 	}
 }
 
-__global__ void Dot(f32 *In1, f32 *In2, f32 *Out, u32 Length) {
-
-#if 0
-	__shared__ u32 SharedMemory[16];
-	u32 GlobalIndex = blockIdx.x * 8 + threadIdx.x;
-	SharedMemory[threadIdx.x + 0] = In1[GlobalIndex];
-	SharedMemory[threadIdx.x + 8] = In2[GlobalIndex];
-	__syncthreads();
-
-	f32 Result = 0.0f;
-	for (u32 i = 0; i < 8; ++i) {
-		Result += SharedMemory[i] * SharedMemory[i + 8];
-	}
-	Out[GlobalIndex] = Result;
-#elif 0
-	__shared__ u32 SharedMemory[16];
-	u32 GlobalIndex = blockIdx.x * 8 + threadIdx.x;
-	SharedMemory[threadIdx.x + 0] = In1[GlobalIndex];
-	SharedMemory[threadIdx.x + 8] = In2[GlobalIndex];
-	__syncthreads();
-
-	SharedMemory[threadIdx.x] = SharedMemory[threadIdx.x] * SharedMemory[threadIdx.x + 8];
-	__syncthreads();
-	SharedMemory[threadIdx.x] += SharedMemory[threadIdx.x + 4];
-	__syncthreads();
-	SharedMemory[threadIdx.x] += SharedMemory[threadIdx.x + 2];
-	__syncthreads();
-	Out[blockIdx.x] = SharedMemory[threadIdx.x] + SharedMemory[threadIdx.x + 1];
-#else
-	u32 GlobalIndex = blockIdx.x * 8 + threadIdx.x;
-	f32 Value = In1[GlobalIndex] * In2[GlobalIndex];
-	for (u32 Offset = 4; Offset > 0; Offset /= 2) {
-		Value += __shfl_down_sync(0xFFFF'FFFF, Value, Offset);
-	}
-	Out[blockIdx.x] = Value;
-#endif
-}
 
 s32 main() {
-	InitRandomIntegers();
-	puts("===");
+	Init();
 
-	f32 *GPUArray1 = 0, *GPUArray2 = 0, *GPUArray3 = 0;
-	cudaMalloc(&GPUArray1, sizeof(InitialArray1));
-	cudaMalloc(&GPUArray2, sizeof(InitialArray2));
-	cudaMalloc(&GPUArray3, sizeof(ResultArray));
-	cudaMemset(GPUArray3, 0, sizeof(f32));
-	cudaMemcpy(GPUArray1, InitialArray1, sizeof(InitialArray1), cudaMemcpyHostToDevice);
-	cudaMemcpy(GPUArray2, InitialArray2, sizeof(InitialArray2), cudaMemcpyHostToDevice);
+	f32 *GPUArrayA = 0, *GPUArrayB = 0, *GPUResult = 0;
+	cudaMalloc(&GPUArrayA, sizeof(InitialArray1));
+	cudaMalloc(&GPUArrayB, sizeof(InitialArray2));
+	cudaMalloc(&GPUResult, sizeof(Result));
+	cudaMemset(GPUResult, 0, sizeof(f32));
+	cudaMemcpy(GPUArrayA, InitialArray1, sizeof(InitialArray1), cudaMemcpyHostToDevice);
+	cudaMemcpy(GPUArrayB, InitialArray2, sizeof(InitialArray2), cudaMemcpyHostToDevice);
 
-	dim3 threadDimension(8, 1);
-	dim3 blockDimension(ARRAY_SIZE / 8, 1);
-	Dot<<<blockDimension, threadDimension>>>(GPUArray1, GPUArray2, GPUArray3, ARRAY_SIZE);
-	cudaMemcpy(ResultArray, GPUArray3, sizeof(ResultArray), cudaMemcpyDeviceToHost);
+	u32 ThreadCount = (ARRAY_SIZE >= 1024) ? 1024 : ARRAY_SIZE;
+	u32 BlockCount = (ARRAY_SIZE + (1023)) / 1024;
+	Dot<<<BlockCount, ThreadCount>>>(GPUArrayA, GPUArrayB, GPUResult);
+	cudaMemcpy(&Result, GPUResult, sizeof(f32), cudaMemcpyDeviceToHost);
 
-	puts("===");
-
-#if 0
-	for (u32 i = 0; i < ARRAY_SIZE; i += 8) {
-		f32 ExpectedResult = 0.0f;
-		for (u32 j = 0; j < 8; j += 1) {
-			ExpectedResult += InitialArray1[i + j] * InitialArray2[i + j];
-		}
-		f32 ActualResult = ResultArray[i / 8];
-		printf("Expected: %.2f -- Actual: %.2f\n", ExpectedResult, ActualResult);
+	f32 ExpectedResult = 0.0f;
+	for (u32 i = 0; i < ARRAY_SIZE; ++i) {
+		ExpectedResult += InitialArray1[i] * InitialArray2[i];
 	}
-#endif
+	f32 ActualResult = Result;
+	if (ExpectedResult == ActualResult) {
+		printf(ANSI_COLOR_GREEN "Expected: %.2f -- Actual: %.2f\n", ExpectedResult, ActualResult);
+	} else {
+		printf(ANSI_COLOR_RED "Expected: %.2f -- Actual: %.2f\n", ExpectedResult, ActualResult);
+	}
+
 	printf(ANSI_COLOR_RESET);
 }
